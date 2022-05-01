24df61fa045577e7e3a2eea7229e29faad51fa78
Farrel Valentino B <92016900+imYours14@users.noreply.github.com>
2022-05-02 01:14:56 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Api\TodoController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within aP group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
