8b10860bb3c1d841fe35eb5e80d8561656ad57a3
Farrel Valentino B <92016900+imYours14@users.noreply.github.com>
2022-05-02 01:39:36 +0700

    PWL_11_Praktikum 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Todo;
 
 class User extends Authenticatable
 {
@@ -42,5 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
